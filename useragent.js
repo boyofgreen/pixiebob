81aa46985c3f10d3044645f8ccf68d01263502f2
Jeff Burtoft <boyofgreen@gmail.com>
2015-08-21 10:59:58 -0500

    add user agent

--- useragent.js
+++ useragent.js
@@ -26,6 +26,6 @@ var webview = document.getElementById('webview');
 
   var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(
                             Windows.Web.Http.HttpMethod.get,
-                            new Windows.Foundation.Uri('http://m.bbc.com/weather');
+                            new Windows.Foundation.Uri('http://m.bbc.com/weather'));
                         httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25")
                         webview.navigateWithHttpRequestMessage(httpRequestMessage);
