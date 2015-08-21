b78fb4526a805c0ee75f1efec5e2ed5b5fb5689d
Jeff Burtoft <boyofgreen@gmail.com>
2015-08-21 12:06:49 -0500

    add user agent

--- useragent.js
+++ useragent.js
@@ -9,9 +9,9 @@ var webview = document.getElementById('webview');
                 // httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25")
                 // WAT.options.webView.navigateWithHttpRequestMessage(httpRequestMessage);
                  
-               // var currentUri = "http://sp.mapfan.com/";
+                var currentUri = "http://sp.mapfan.com/";
                 webview.addEventListener("MSWebViewNavigationStarting", function (e) {
-                  //  if (currentUri != e.uri) {
+                    if (currentUri != e.uri) {
                         e.preventDefault();
                         
                         e.stopImmediatePropagation();
@@ -19,10 +19,11 @@ var webview = document.getElementById('webview');
                         var currentUri = e.uri;
                         var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(
                             Windows.Web.Http.HttpMethod.get,
-                            new Windows.Foundation.Uri(currentUri));
+                            new Windows.Foundation.Uri(e.uri));
                         httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25")
                         webview.navigateWithHttpRequestMessage(httpRequestMessage);
-                   // }
+                        
+                    }
                 });
 
 //webview.navigate();
