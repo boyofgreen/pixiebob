6dfa4be1258324851c9620028c4c1f44be1a2b4f
Jeff Burtoft <boyofgreen@gmail.com>
2015-08-21 12:58:19 -0500

    tourble shoot ua string

--- useragent.js
+++ useragent.js
@@ -18,7 +18,7 @@ var webview = document.getElementById('webview');
                         var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(
                             Windows.Web.Http.HttpMethod.get,
                             new Windows.Foundation.Uri(e.uri));
-                        httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25")
+                        httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25")
                         webview.navigateWithHttpRequestMessage(httpRequestMessage);
                         
                     }
@@ -29,5 +29,5 @@ var webview = document.getElementById('webview');
   var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(
                             Windows.Web.Http.HttpMethod.get,
                             new Windows.Foundation.Uri('http://m.bbc.com/weather'));
-                        httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25")
+                        httpRequestMessage.headers.userAgent.tryParseAdd("Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25")
                         webview.navigateWithHttpRequestMessage(httpRequestMessage);
